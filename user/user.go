9da68f07eff5d155cc9f8778658c51c70d37774f
Manuel de la Peña <mdelapenya@gmail.com>
2024-05-01 09:33:08 +0200

    chore: user UUID

--- user/user.go
+++ user/user.go
@@ -3,14 +3,16 @@ package user
 import (
 	"database/sql"
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 type UserInfo struct {
-	UserID int `json:"UserID"`
-	Age    int `json:"Age"`
+	UserID uuid.UUID `json:"UserID"`
+	Age    int       `json:"Age"`
 }
 
-func GetInfo(db *sql.DB, userID int) (UserInfo, error) {
+func GetInfo(db *sql.DB, userID uuid.UUID) (UserInfo, error) {
 	var ui UserInfo
 	var age int
 	err := db.QueryRow("SELECT age FROM users WHERE id = $1", userID).Scan(&age)
